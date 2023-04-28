bd1870f0167bda719149b604a642d0d0f324e9e0
derwind <uncertainty_principle_ss@yahoo.co.jp>
2023-04-29 00:02:37 +0900

    implement stub operator-overloads

--- mynumpy/core/ndarray.py
+++ mynumpy/core/ndarray.py
@@ -2,6 +2,18 @@ class ndarray():
     def __init__(self, data):
         self.data = data
 
+    def __str__(self):
+        return str(self.data)
+
+    def __repr__(self):
+        return str(self.data)
+
+    def __eq__(self, other):
+        ...
+
+    def  __ne__(self, other):
+        ...
+
     def __add__(self, other):
         ...
 
@@ -11,5 +23,27 @@ class ndarray():
     def __mul__(self, other):
         ...
 
+    def __matmul__(self, other):
+        ...
+
     def __truediv__(self, other):
+        ...
+
+    @property
+    def ndim(self):
+        ...
+
+    @property
+    def shape(self):
+        ...
+
+    @property
+    def size(self):
+        ...
+
+    @property
+    def T(self):
+        ...
+
+    def reshape(self, shape):
         ...
