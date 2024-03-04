4537b0642bd0547721e380ff6477304dc16c66d8
derwind <uncertainty_principle_ss@yahoo.co.jp>
2024-03-04 22:56:34 +0900

    add 'eye'

--- mynumpy/core/array.py
+++ mynumpy/core/array.py
@@ -4,14 +4,22 @@ import copy as copy_
 from typing import List
 
 from ..dtypes import Numbers
-from .ndarray import calc_shape, is_number, ndarray, _guess_dtype
+from .ndarray import calc_shape, is_number, ndarray, zeros, _guess_dtype
 
 
 def array(data: List[Numbers], dtype=None, *, copy: bool = True) -> ndarray:
-    import mynumpy as mynp
-
     if copy:
         data = copy_.deepcopy(data)
     if dtype is None:
         dtype = _guess_dtype(data)
-    return mynp.ndarray(calc_shape(data), dtype, data)
+    return ndarray(calc_shape(data), dtype, data)
+
+
+def eye(N: int, M: int | None = None, dtype=float) -> ndarray:
+    if M is None:
+        M = N
+    arr = zeros((N, M), dtype=dtype)
+    one = dtype(1)
+    for i in range(min(N, M)):
+        arr.data[i, i] = one
+    return arr
