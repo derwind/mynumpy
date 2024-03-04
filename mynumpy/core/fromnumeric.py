4242537028a0b85312710dba9b1d4e2900dcc5d9
derwind <uncertainty_principle_ss@yahoo.co.jp>
2024-03-05 02:09:07 +0900

    implement 'allclose'

--- mynumpy/core/fromnumeric.py
+++ mynumpy/core/fromnumeric.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Sequence
 
 from ..dtypes import Numbers
-from .ndarray import is_number
+from .ndarray import is_number, ndarray
 
 
 def prod(a: Sequence[Numbers], dtype: type | None = None):
@@ -17,3 +17,14 @@ def prod(a: Sequence[Numbers], dtype: type | None = None):
     for n in a:
         result *= n
     return dtype(result)
+
+
+def allclose(a: ndarray, b: ndarray, rtol=1.0e-5, atol=1.0e-8):
+    if a.shape != b.shape:
+        return False
+    a = a.flatten()
+    b = b.flatten()
+    for v1, v2 in zip(a, b):
+        if abs(v1 - v2) > atol + rtol * abs(v2):
+            return False
+    return True
