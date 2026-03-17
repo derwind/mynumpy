f84169b6af6705e5eb09e2767199d4c00ce48535
derwind <uncertainty_principle_ss@yahoo.co.jp>
2026-03-17 16:13:54 +0000

    fix `ord` for `ord == 1`

--- mynumpy/linalg/linalg.py
+++ mynumpy/linalg/linalg.py
@@ -23,6 +23,11 @@ def norm(x: list | ndarray, ord: int | None = None) -> float:
     elif ord <= 0 or ord > 2:
         raise ValueError("only ord=1 and ord=2 are supported")
 
+    # XXX
+    if ord == 1 and isinstance(x, list) and isinstance(x[0], list):
+        col_sums = [sum(abs(row[j]) for row in x) for j in range(len(x[0]))]
+        return max(col_sums)
+
     def walk(data, total):
         if is_number(data):
             total[0] += abs(data) if ord == 1 else abs(data) ** ord
